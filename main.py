c20f59d7a837259440201e8f086be79b6b32f598
mariamatusiskova <xmatusiskova@stuba.sk>
2023-09-30 23:15:39 +0200

    Second part of the project in the beginning

--- main.py
+++ main.py
@@ -28,6 +28,116 @@ class Packet:
         data = constructor.construct_mapping(node, deep=True)
         return cls(**data)
 
+
+######################################## change
+# find ether type
+def findUDPProtocol(hex_pck):
+    if int(hex_pck[24:28], 16) == 37:
+        return 'TIME'
+    elif int(hex_pck[24:28], 16) == 53:
+        return 'DNS'
+    elif int(hex_pck[24:28], 16) == 67 or int(hex_pck[24:28], 16) == 68:
+        return 'DHCP'
+    elif int(hex_pck[24:28], 16) == 69:
+        return 'TFTP'
+    elif int(hex_pck[24:28], 16) == 80:
+        return 'HTTP'
+    elif int(hex_pck[24:28], 16) == 137:
+        return 'NETBIOS-NS'
+    elif int(hex_pck[24:28], 16) == 138:
+        return 'NETBIOS-DGM'
+    elif int(hex_pck[24:28], 16) == 161:
+        return 'SNMP'
+    elif int(hex_pck[24:28], 16) == 162:
+        return 'SNMP-TRAP'
+    elif int(hex_pck[24:28], 16) == 443:
+        return 'HTTPS'
+    elif int(hex_pck[24:28], 16) == 514:
+        return 'SYSLOG'
+    elif int(hex_pck[24:28], 16) == 520:
+        return 'RIP'
+    elif int(hex_pck[24:28], 16) == 1900:
+        return 'SSDP'
+    elif int(hex_pck[24:28], 16) == 5353:
+        return 'MDNS'
+    elif int(hex_pck[24:28], 16) == 17500:
+        return 'DB-LSP-DISC'
+    elif int(hex_pck[24:28], 16) == 33434:
+        return 'TRACEROUTE'
+
+
+######################################## change
+# find ether type
+def findTCPProtocol(hex_pck):
+    if int(hex_pck[24:28], 16) == 20:
+        return 'FTP-DATA'
+    elif int(hex_pck[24:28], 16) == 21:
+        return 'FTP-CONTROL'
+    elif int(hex_pck[24:28], 16) == 22:
+        return 'SSH'
+    elif int(hex_pck[24:28], 16) == 23:
+        return 'TELNET'
+    elif int(hex_pck[24:28], 16) == 25:
+        return 'SMTP'
+    elif int(hex_pck[24:28], 16) == 53:
+        return 'DNS'
+    elif int(hex_pck[24:28], 16) == 80:
+        return 'HTTP'
+    elif int(hex_pck[24:28], 16) == 110:
+        return 'POP3'
+    elif int(hex_pck[24:28], 16) == 119:
+        return 'NNTP'
+    elif int(hex_pck[24:28], 16) == 137:
+        return 'NETBIOS-NS'
+    elif int(hex_pck[24:28], 16) == 139:
+        return 'NETBIOS-SSN'
+    elif int(hex_pck[24:28], 16) == 143:
+        return 'IMAP'
+    elif int(hex_pck[24:28], 16) == 162:
+        return 'SNMP-TRAP'
+    elif int(hex_pck[24:28], 16) == 179:
+        return 'BGP'
+    elif int(hex_pck[24:28], 16) == 389:
+        return 'LDAP'
+    elif int(hex_pck[24:28], 16) == 443:
+        return 'HTTPS'
+    elif int(hex_pck[24:28], 16) == 514:
+        return 'SYSLOG'
+    elif int(hex_pck[24:28], 16) == 17500:
+        return 'DB-LSP-DISC'
+
+
+
+######################################## change
+# find ether type
+def findIPv4Protocol(hex_pck):
+    if int(hex_pck[24:28], 16) == 0x01:
+        return 'ICMP'
+    elif int(hex_pck[24:28], 16) == 0x02:
+        return 'IGMP'
+    elif int(hex_pck[24:28], 16) == 0x06:
+        return 'TCP'
+    elif int(hex_pck[24:28], 16) == 0x11:
+        return 'UDP'
+    elif int(hex_pck[24:28], 16) == 0x67:
+        return 'PIM'
+
+
+########################################
+# find ether type
+def findEtherTypeEthernet(hex_pck):
+    if int(hex_pck[24:28], 16) == 0x0800:
+        return 'IPv4'
+    elif int(hex_pck[24:28], 16) == 0x0806:
+        return 'ARP'
+    elif int(hex_pck[24:28], 16) == 0x88CC:
+        return 'LLDP'
+    elif int(hex_pck[24:28], 16) == 0x86DD:
+        return 'IPv6'
+    elif int(hex_pck[24:28], 16) == 0x9000:
+        return 'ECTP'
+
+
 # find correct protocol for IEEE 802.3 LLC & SNAP
 def findPid(hex_pck):
     if int(hex_pck[40:44], 16) == 0x2000:
@@ -39,6 +149,7 @@ def findPid(hex_pck):
     elif int(hex_pck[40:44], 16) == 0x809B:
         return 'AppleTalk'
 
+
 # find correct protocol IEEE 802.3 LLC
 def findSap(hex_pck):
     if int(hex_pck[28:30], 16) == 0x42:
@@ -47,6 +158,7 @@ def findSap(hex_pck):
         return 'IPX'
     elif int(hex_pck[28:30], 16) == 0xF0:
         return 'NETBIOS'
+
 
 # transform to hex data of the packet
 def printHexData(hex_pck):
@@ -62,6 +174,7 @@ def printHexData(hex_pck):
             hex_data += "\n"
 
     return hex_data.upper().strip()
+
 
 # list frames into yaml, main logic
 def listOfFrames(file_name):
@@ -79,8 +192,10 @@ def listOfFrames(file_name):
         sap_value = None
         pid_value = None
 
+        # beginning index normal, last one + 1
         if int(hex_pck[24:28], 16) >= 0x5DC:
             define_frame_type = 'Ethernet II'
+            define_ether_type = None
         else:
             if int(hex_pck[28:32], 16) == 0xFFFF:
                 define_frame_type = 'IEEE 802.3 RAW'
@@ -99,6 +214,7 @@ def listOfFrames(file_name):
             frame_type=define_frame_type,
             src_mac=':'.join(hex_pck[:12][i:i + 2] for i in range(0, len(hex_pck[:12]), 2)),
             dst_mac=':'.join(hex_pck[12:24][i:i + 2] for i in range(0, len(hex_pck[12:24]), 2)),
+            ether_type=None,
             sap=sap_value,
             pid=pid_value,
             hexa_frame=scalarstring.PreservedScalarString(printHexData(hex_pck))
