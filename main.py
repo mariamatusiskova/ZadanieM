1710ca4370ea32fd9e4b73f0af51fa0c1c91d44d
mariamatusiskova <xmatusiskova@stuba.sk>
2023-09-30 13:17:24 +0200

    in progress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ruamel import yaml
+from ruamel.yaml import scalarstring
 from scapy.all import *
 from binascii import hexlify
 import ruamel.yaml
@@ -14,6 +15,9 @@ class Packet:
 
     @classmethod
     def to_yaml(cls, representer, node):
+        for key, value in list(node.kwargs.items()):
+            if value is None:
+                node.kwargs.pop(key)
         return representer.represent_mapping(cls.yaml_tag, node.kwargs)
 
     @classmethod
@@ -23,24 +27,24 @@ class Packet:
         return cls(**data)
 
 
-def printPid(hex_pck):
+def findPid(hex_pck):
     if int(hex_pck[40:44], 16) == 0x2000:
-        print(f"pid: CDP")
+        return 'CDP'
     elif int(hex_pck[40:44], 16) == 0x2004:
-        print(f"pid: DTP")
+        return 'DTP'
     elif int(hex_pck[40:44], 16) == 0x010B:
-        print(f"pid: PVSTP+")
+        return 'PVSTP+'
     elif int(hex_pck[40:44], 16) == 0x809B:
-        print(f"pid: AppleTalk")
+        return 'AppleTalk'
 
 
-def printSap(hex_pck):
+def findSap(hex_pck):
     if int(hex_pck[28:30], 16) == 0x42:
-        print(f"sap: STP")
+        return 'STP'
     elif int(hex_pck[28:30], 16) == 0xE0:
-        print(f"sap: IPX")
+        return 'IPX'
     elif int(hex_pck[28:30], 16) == 0xF0:
-        print(f"sap: NETBIOS")
+        return 'NETBIOS'
 
 
 def printHexData(hex_pck):
@@ -48,57 +52,84 @@ def printHexData(hex_pck):
     num_pairs = 0
     for i in range(0, len(hex_pck), 2):
         hex_data += hex_pck[i] + hex_pck[i + 1]
-        hex_data += " "
         num_pairs += 1
+        if num_pairs < 15:
+            hex_data += " "
         if num_pairs == 15:
             num_pairs = 0
             hex_data += "\n"
 
-    hex_data = hex_data.upper()
-    return hex_data
+
+    return hex_data.upper()
 
 
 def listOfFrames():
     file = rdpcap('../vzorky_pcap_na_analyzu/trace-26.pcap')
 
-    for frame_num, pck in enumerate(file, start=0):
+    # Initialize the begin dictionary
+    begin = {
+        "name": "PKS2023/24",
+        "pcap_name": "eth-1.pcap",
+        "packets": []
+    }
 
-        pck_data = Packet(
-            frame_number=frame_num,
-            len_frame_pcap=len(pck),
-            # +4 because of FCS
-            len_frame_medium=max(64, len(pck) + 4)
-        )
+    # Open the YAML file in write mode
+    with open('yaml_file.yml', 'w') as yaml_file:
+        # Write the begin dictionary at the beginning of the file
+        yaml = ruamel.yaml.YAML()
+        yaml.dump(begin, yaml_file)
 
-        # transform code to raw data of wireshark
-        # hexlify bytes and convert them to the string
-        hex_pck = hexlify(bytes(pck)).decode('utf-8')
+    with open('yaml_file.yml', 'a') as yaml_file:
 
-        sap = False
-        pid = False
+        for frame_num, pck in enumerate(file, start=0):
 
-        if int(hex_pck[24:28], 16) >= 0x5DC:
-            print(f"frame_type: Ethernet II")
-        else:
-            if int(hex_pck[28:32], 16) == 0xFFFF:
-                print(f"frame_type: IEEE 802.3 RAW")
-            elif int(hex_pck[28:32], 16) == 0xAAAA:
-                print(f"frame_type: IEEE 802.3 LLC & SNAP")
-                pid = True
+            # transform code to raw data of wireshark
+            # hexlify bytes and convert them to the string
+            hex_pck = hexlify(bytes(pck)).decode('utf-8')
+
+            sap_value = None
+            pid_value = None
+
+            if int(hex_pck[24:28], 16) >= 0x5DC:
+                define_frame_type = 'Ethernet II'
             else:
-                print(f"frame_type: IEEE 802.3 LLC")
-                sap = True
+                if int(hex_pck[28:32], 16) == 0xFFFF:
+                    define_frame_type = 'IEEE 802.3 RAW'
+                elif int(hex_pck[28:32], 16) == 0xAAAA:
+                    define_frame_type = 'IEEE 802.3 LLC & SNAP'
+                    pid_value = findPid(hex_pck)
+                else:
+                    define_frame_type = 'IEEE 802.3 LLC'
+                    sap_value = findSap(hex_pck)
 
-        dst_addr = ':'.join(hex_pck[:12][i:i + 2] for i in range(0, len(hex_pck[:12]), 2))
-        src_addr = ':'.join(hex_pck[12:24][i:i + 2] for i in range(0, len(hex_pck[12:24]), 2))
+            pck_data = Packet(
+                frame_number=frame_num,
+                len_frame_pcap=len(pck),
+                # +4 because of FCS
+                len_frame_medium=max(64, len(pck) + 4),
+                frame_type=define_frame_type,
+                src_mac=':'.join(hex_pck[:12][i:i + 2] for i in range(0, len(hex_pck[:12]), 2)),
+                dst_mac=':'.join(hex_pck[12:24][i:i + 2] for i in range(0, len(hex_pck[12:24]), 2)),
+                sap=sap_value,
+                pid=pid_value,
+                hexa_frame=scalarstring.PreservedScalarString(printHexData(hex_pck))
+            )
 
-        print(f"src_mac: {src_addr}")
-        print(f"dst_mac: {dst_addr}")
+            # yaml_format = ruamel.yaml.YAML()
+            # yaml_format.register_class(Packet)
+            # yaml_format.dump([pck_data], yaml_file)
+            yaml_format = ruamel.yaml.YAML()
+            yaml_format.register_class(Packet)
+            begin["packets"].append(pck_data)
 
-        if sap:
-            printSap(hex_pck)
-        elif pid:
-            printPid(hex_pck)
+            # Rewrite the entire YAML file with the updated begin dictionary
+            yaml_format.dump(begin, yaml_file)
+
+
+
+    # with open(r'yaml_file.yml', 'w') as yaml_file:
+    #     yaml.dump(begin, yaml_file, default_flow_style=False)
+    #     #yaml_format.dump([pck_data], sys.stdout)
 
         # print(f"hexa_frame: | \n{printHexData(hex_pck)}\n")
         # 'hexa_frame': ruamel.yaml.scalaring.LiteralScalarString(printHexData(hex_pck))
@@ -106,14 +137,3 @@ def listOfFrames():
 
 if __name__ == '__main__':
     listOfFrames()
-
-    pck_data = Packet(
-        frame_number=1,
-        frame_type='ETHERNET II',
-        src_mac='00:02:CF:AB:A2:4C',
-        dst_mac='B4:B5:2F:74:CB:AE'
-    )
-
-    yaml = ruamel.yaml.YAML()
-    yaml.register_class(Packet)
-    yaml.dump([pck_data], sys.stdout)
