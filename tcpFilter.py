d42eea344c2c935552f8d97db71d1ba1108f9d09
mariamatusiskova <xmatusiskova@stuba.sk>
2023-10-21 20:43:18 +0200

    tcp fixed, i hope, really

--- tcpFilter.py
+++ tcpFilter.py
@@ -20,34 +20,45 @@ def isValid(pck, protocol):
             'protocol'] == "TCP" and pck.kwargs['app_protocol'] == protocol
 
 
-def isComplete(open_num, finish_num, group_list, first_incomplete):
+def isComplete(finish_num, group_list, starting_packet_list):
     final_group_list = []
 
-    if open_num <= -1 and finish_num <= -1:
-        return [], "empty"
-    elif (open_num >= -1 and finish_num <= -1) or (open_num <= -1 and finish_num >= -1):
+    for pck in group_list:
+        frame_number = pck.kwargs['frame_number']
+        if starting_packet_list[0].kwargs['frame_number'] <= frame_number <= finish_num:
+            final_group_list.append(pck.kwargs)
+
+    return final_group_list
+
+
+def isIncomplete(finish_num, group_list, first_incomplete, starting_packet_list):
+    final_group_list = []
+
+    if (starting_packet_list[0].kwargs['frame_number'] >= -1 and finish_num <= -1) or (starting_packet_list[0].kwargs['frame_number'] <= -1 and finish_num >= -1):
         first_incomplete += 1
 
         if first_incomplete == 1:
             for pck in group_list:
                 frame_number = pck.kwargs['frame_number']
-                if -1 <= open_num and frame_number >= open_num and finish_num <= -1:
+                if -1 <= starting_packet_list[0].kwargs['frame_number'] and frame_number >= starting_packet_list[0].kwargs['frame_number'] and finish_num <= -1:
                     final_group_list.append(pck.kwargs)
-                elif finish_num >= -1 and frame_number <= finish_num and open_num <= -1:
+                elif finish_num >= -1 and frame_number <= finish_num and starting_packet_list[0].kwargs['frame_number'] <= -1:
                     final_group_list.append(pck.kwargs)
 
-        return final_group_list, "incomplete"
-
-    for pck in group_list:
-        frame_number = pck.kwargs['frame_number']
-        if open_num <= frame_number <= finish_num:
-            final_group_list.append(pck.kwargs)
-
-    return final_group_list, "complete"
+        return final_group_list
 
 
+def checkFullValidEnd(counter, finishing_list, rst):
+    return (counter == 4 and (len(finishing_list) == 6 and finishing_list[0:6] == ["FIN", "ACK", "ACK", "FIN", "ACK", "ACK"])) or (counter == 4 and (len(finishing_list) == 6 and finishing_list[0:6] == ["FIN", "ACK", "FIN", "ACK", "ACK", "ACK"])) or rst in finishing_list
 
-def checkFinishFlags(binary_data, end_list, frame_num, counter):
+
+def checkFullValidShorterEnd(counter, finishing_list, frame_num, group):
+    return (counter == 3 and (len(finishing_list) == 5 and finishing_list[0:5] == ["FIN", "ACK", "ACK", "RST", "ACK"]) and frame_num == group[len(group) - 1].kwargs['frame_number']) or (counter == 3 and (len(finishing_list) == 5 and finishing_list[0:5] == ["FIN", "ACK", "ACK", "FIN", "ACK"]) and frame_num == group[len(group) - 1].kwargs['frame_number']) or (counter == 3 and (len(finishing_list) == 5 and finishing_list[0:5] == ["FIN", "ACK", "FIN", "ACK", "ACK"]) and frame_num == group[len(group) - 1].kwargs['frame_number'])
+
+def invalidEnd(finishing_list):
+    return len(finishing_list) == 6 and (finishing_list[0:6] != ["FIN", "ACK", "ACK", "FIN", "ACK", "ACK"]) or (len(finishing_list) == 6 and finishing_list[0:6] != ["FIN", "ACK", "FIN", "ACK", "ACK", "ACK"])
+
+def checkFinishFlags(binary_data, end_list, frame_num, counter, group):
     if end_list:
         flag_list = end_list.copy()
     else:
@@ -85,26 +96,34 @@ def checkFinishFlags(binary_data, end_list, frame_num, counter):
         finishing_list = flag_list.copy()
         counter += 1
 
-    if counter == 4 and (len(finishing_list) == 6 and finishing_list[0:6] == ["FIN", "ACK", "ACK", "FIN", "ACK", "ACK"] or finishing_list[0:6] == ["FIN", "ACK", "FIN", "ACK", "ACK", "ACK"]) or rst in finishing_list:
+    if checkFullValidEnd(counter, finishing_list, rst):
         pck_num = frame_num
         counter = 0
         return pck_num, finishing_list.clear(), counter
-    elif counter == 3 and (len(finishing_list) == 5 and finishing_list[0:5] == ["FIN", "ACK", "ACK", "RST", "ACK"]):
-        pck_num = frame_num - counter
+    elif checkFullValidShorterEnd(counter, finishing_list, frame_num, group):
+        pck_num = frame_num
         counter = 0
-        return pck_num, finishing_list.clear(), counter
-    elif len(finishing_list) == 6 and (finishing_list[0:6] != ["FIN", "ACK", "ACK", "FIN", "ACK", "ACK"]) or (len(finishing_list) == 6 and finishing_list[0:6] != ["FIN", "ACK", "FIN", "ACK", "ACK", "ACK"]):
+        return pck_num, finishing_list, counter
+    elif invalidEnd(finishing_list):
+        counter = 0
+        return -1, finishing_list.clear(), counter
+    elif counter > 4:
         counter = 0
         return -1, finishing_list.clear(), counter
     else:
         return -1, finishing_list, counter
 
 
-def checkStartFlags(binary_data, start_list, frame_num, counter):
+def checkStartFlags(binary_data, start_list, frame_num, counter, packet, packet_start_list):
     if start_list:
         flag_list = start_list.copy()
     else:
         flag_list = []
+
+    if packet_start_list:
+        save_packet_list = packet_start_list.copy()
+    else:
+        save_packet_list = []
 
     starting_list = []
     fin = "FIN"
@@ -134,21 +153,19 @@ def checkStartFlags(binary_data, start_list, frame_num, counter):
         count_flags += 1
 
     if count_flags > 0 and flag_list and (flag_list[0] == "SYN"):
+        save_packet_list.append(packet)
         starting_list = flag_list.copy()
         counter += 1
 
     if (counter == 3 or counter == 4) and (len(starting_list) == 4 and starting_list[0:4] == ["SYN", "SYN", "ACK", "ACK"]):
-        if counter == 3 or counter == 4:
-            pck_num = frame_num - counter
-        else:
-            pck_num = frame_num
+        pck_num = frame_num
         counter = 0
-        return pck_num, starting_list.clear(), counter
+        return pck_num, starting_list.clear(), counter, save_packet_list
     elif len(flag_list) == 4 and flag_list[0:4] != ["SYN", "SYN", "ACK", "ACK"]:
         counter = 0
-        return -1, starting_list.clear(), counter
+        return -1, starting_list.clear(), counter, packet_start_list.clear()
     else:
-        return -1, starting_list, counter
+        return -1, starting_list, counter, save_packet_list
 
 
 def parseHexData(hex_pck):
@@ -208,6 +225,7 @@ def getProtocolList(packet_list, protocol, file_name):
     in_comm_list = []
     count_communications = 0
     for group in groups_list:
+        save_starting_packet = []
         open_comm_record = -1
         src_comm = None
         dst_comm = None
@@ -222,16 +240,16 @@ def getProtocolList(packet_list, protocol, file_name):
                 src_comm = packet.kwargs['src_ip']
                 dst_comm = packet.kwargs['dst_ip']
 
-            open_comm, start, counter_start = checkStartFlags(binary_data, start, packet.kwargs['frame_number'], counter_start)
-            finish_comm, end, counter_finish = checkFinishFlags(binary_data, end, packet.kwargs['frame_number'], counter_finish)
+            open_comm, start, counter_start, save_starting_packet = checkStartFlags(binary_data, start, packet.kwargs['frame_number'], counter_start, packet, save_starting_packet)
+            finish_comm, end, counter_finish = checkFinishFlags(binary_data, end, packet.kwargs['frame_number'], counter_finish, group)
 
             if open_comm > -1:
                 open_comm_record = open_comm
 
             if open_comm_record > -1 and finish_comm > -1:
-                final_group_list, status = isComplete(open_comm_record, finish_comm, group, count_incomplete)
+                final_group_list = isComplete(finish_comm, group, save_starting_packet)
 
-                if final_group_list and status == "complete":
+                if final_group_list:
                     count_communications += 1
 
                     comm_data = Packet(
@@ -242,20 +260,38 @@ def getProtocolList(packet_list, protocol, file_name):
                     )
                     yaml_format.register_class(Packet)
                     comm_list.append(comm_data)
-                open_comm_record = 0
+                open_comm_record = -1
+                finish_comm = -1
+                save_starting_packet.clear()
 
-        final_group_list, status = isComplete(open_comm, finish_comm, group, count_incomplete)
+            if open_comm_record == -1 and finish_comm > -1:
+                final_group_list = isIncomplete(finish_comm, group, count_incomplete, save_starting_packet)
 
-        if final_group_list and status == "incomplete":
-            count_incomplete += 1
-            if count_incomplete == 1:
-                comm_data = Packet(
-                    number_comm=count_incomplete,
-                    packets=final_group_list
-                )
-                yaml_format.register_class(Packet)
-                in_comm_list.append(comm_data)
+                if final_group_list:
+                    count_incomplete += 1
+                    if count_incomplete == 1:
+                        comm_data = Packet(
+                            number_comm=count_incomplete,
+                            packets=final_group_list
+                        )
+                        yaml_format.register_class(Packet)
+                        in_comm_list.append(comm_data)
+                finish_comm = -1
+                save_starting_packet.clear()
 
+        if open_comm_record > -1 and finish_comm == -1:
+            final_group_list = isIncomplete(finish_comm, group, count_incomplete, save_starting_packet)
+
+            if final_group_list:
+                count_incomplete += 1
+                if count_incomplete == 1:
+                    comm_data = Packet(
+                        number_comm=count_incomplete,
+                        packets=final_group_list
+                    )
+                    yaml_format.register_class(Packet)
+                    in_comm_list.append(comm_data)
+            save_starting_packet.clear()
 
     header = {
         "name": "PKS2023/24",
